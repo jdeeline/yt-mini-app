fbc7b47a82db33424b27d300cac90823bffd43e6
jdeeline <jdeeline@gmail.com>
2024-08-17 19:47:08 +0700

    Update styles

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,15 +1,8 @@
 import CreateVideo from '@components/CreateVideo/CreateVideo';
 
 function Home() {
-  const containerStyles = {
-    height: '100%',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  };
-
   return (
-    <div style={containerStyles}>
+    <div className="centered-content">
       <CreateVideo />
     </div>
   );
